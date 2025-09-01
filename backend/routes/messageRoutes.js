e8e2019aca3615e2caf1f82f712ddaa7583db88a
Greetika <bajracharyagreetika1@gmail.com>
2025-09-01 15:47:44 +0545

    message routes,controllers and models added

---
+++ backend/routes/messageRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import { getUsersForSidebar, getMessages, markMessageAsSeen, sendMessage} from "../controllers/messageController.js";
+import { protectRoute } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.put("/mark/:id", protectRoute, markMessageAsSeen);
+router.post("/send/:id", protectRoute, sendMessage);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,39 @@ import http from "http";
 import connectDB from "./lib/db.js";
 
 import userRoutes from "./routes/userRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+
+import { Server } from "socket.io";
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+
+export const io = new Server(server, {
+  cors: {
+    origin: "*"
+  },
+});
+
+export const userSocketMap = {};
+
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+  console.log("User connected:", userId);
+
+  if(userId) {
+    userSocketMap[userId] = socket.id;
+  }
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 app.use(express.json({ limit: "4mb"}));
 app.use(cors());
@@ -17,6 +45,7 @@ app.use(cors());
 app.use("/api/status", (req, res) => {res.send("server is live");}) ;
 
 app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoutes);
 
 await connectDB();
 
