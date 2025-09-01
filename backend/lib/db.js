87b7bc3b8076116f7de2911be1fb88351a243db4
Greetika <bajracharyagreetika1@gmail.com>
2025-09-01 21:07:31 +0545

    utils

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () => {
     });
     await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
+    console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
